3fb9c9a675a3d1a6e7f295e3cbd0331893745997
Allen <allenuwe16@gmail.com>
2026-05-20 18:02:47 +0530

    fix vercel backend build

--- artifacts/api-server/api/index.ts
+++ artifacts/api-server/api/index.ts
@@ -1,5 +1,6 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import app from "../src/app";
+// import app from "../src/app";
+import app from "../src/app.js"
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   // Patch request path for Express routing under /api
